cec1c90958c4e749731d2e8c6d41ef20e5394033
kermfiratt49 <keremfiratt49@gmail.com>
2025-03-24 03:33:48 +0300

    thecommit

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ const PORT = process.env.PORT || 5000; // Use Heroku's port or default to 5000
 app.use(cors());
 app.use(express.json());
 
+// Redirect HTTP to HTTPS
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
+
+// Redirect root domain (finfact.io) to www.finfact.io
+app.use((req, res, next) => {
+  if (req.hostname === 'finfact.io') {
+    res.redirect(`https://www.finfact.io${req.url}`);
+  } else {
+    next();
+  }
+});
+
 // Serve static files from the React app (after building)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
